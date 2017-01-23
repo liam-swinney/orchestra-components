b1259857e9e4c989297da2ef39aec79915a8a06d
Liam Swinney <liam.swinney@bedegaming.com>
2017-01-23 14:32:04 +0000

    added babel polypill and webcomponents/custom elements polyfil

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import 'babel-polyfill/dist/polyfill';
+import '@webcomponents/custom-elements';
+
 export { Component } from './components/component';
 export { Page } from './components/page';
 export { Router } from './components/router';
